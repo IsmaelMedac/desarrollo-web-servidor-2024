24d9a4ec63a95b1fcc61507a2391782b1153bc35
IsmaelMedac <ips0013@alu.medac.es>
2024-10-08 14:47:25 +0200

    subir

--- 04_formularios/edades.php
+++ 04_formularios/edades.php
@@ -16,7 +16,33 @@
 
     Hacer la lógica con la estructura match
 -->
+    <form action="" method="post">
 
+    <input type="text" name="nombre"><br><br>
+    <input type="text" name="edad"><br><br>
+    <input type="submit" value="comprobar"><br><br>
 
+    </form>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $nombre = $_POST["nombre"];
+            $edad = $_POST["edad"];
+
+            $res = match (true) {
+                $edad < 18 => ""
+                 => 
+            }
+        }
+        
+    ?>
+
+    <!--
+        CREAR CON NUMEROS ALEATORIOS UN ARRAY CON 10 ENTEROS DEL 1 AL 50
+        MOSTRAR DICHOS NUMEROS DE LA FORMA QUE MAS NOS GUSTE
+        CREAR UNN FORMULARIO DONDE SE INTENTE INTRODUCIR EL MAXIMO VALOR
+        Y SE COMPRUEBE SI HAS ACERTADO
+
+    -->
 </body>
 </html>
