2e0841fb2f2d399aa6bf48a9fe512c9ee31f6dcd
Ismael <159699261+IsmaelMedac@users.noreply.github.com>
2024-10-17 17:35:58 +0200

    a

--- 04_formularios/convertir_divisas.php
+++ 04_formularios/convertir_divisas.php
@@ -32,28 +32,28 @@
             define("DOLAR", 1.18);
             define("YEN", 129.852);
             
-            $cantidad = $_POST["cantidad"];
+            $cantidad = (float)$_POST["cantidad"];
             $opciones_convertir = $_POST["opciones_convertir"];
             $opciones_cambio = $_POST["opciones_cambio"];
             //se hace la conversion de monedas
             if($opciones_convertir == "€" && $opciones_cambio == "$"){
                 $resultado = $cantidad * DOLAR;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Dolares";
             }elseif($opciones_convertir == "€" && $opciones_cambio == "Y"){
                 $resultado = $cantidad * YEN;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Yenes";
             }elseif($opciones_convertir == "$" && $opciones_cambio == "€"){
                 $resultado = $cantidad / DOLAR;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Euros";
             }elseif($opciones_convertir == "$" && $opciones_cambio == "Y"){
                 $resultado = $cantidad * YEN / DOLAR;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Yenes";
             }elseif($opciones_convertir == "Y" && $opciones_cambio == "€"){
                 $resultado = $cantidad / YEN;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Euros";
             }elseif($opciones_convertir == "Y" && $opciones_cambio == "$"){
                 $resultado = $cantidad * DOLAR / YEN;
-                echo "El resultado es: $resultado";
+                echo "El resultado es: $resultado Dolares";
             }else{
                 echo "No se puede convertir la misma moneda";
             }
